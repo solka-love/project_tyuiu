89061e870f35645c4096bfd55d0967ba2d189b64
Andrey Gorobtzov <92875957+AndreyGorobtzov@users.noreply.github.com>
2023-10-20 22:10:55 +0500

    Add files via upload

--- EduTyuiu/js/main.js
+++ EduTyuiu/js/main.js
@@ -1,4 +1,5 @@
-function adaptive_function(){
+function adaptive()
+{
     if  (document.body.clientWidth <= document.body.clientHeight) {
         document.getElementById("courses").style.gridTemplateColumns = "repeat(2, 40vw)";
         document.getElementById("courses").style.height = "71vmax";
@@ -9,6 +10,35 @@ function adaptive_function(){
     }
 }
 
-// Бессмысленная и беспощадная адаптивность
-document.addEventListener("DOMContentLoaded", adaptive_function);
-window.addEventListener("resize", adaptive_function);
+function user_status()
+{
+    // Проверка юзера на его статус (препод/студент). Мне нужны роли
+    if (1) {
+        var_ref = document.getElementById("var_ref");
+        var_ref.style.backgroundImage = 'url("img/+.svg")';
+        var_ref.style.visibility = "visible";
+        var_ref.href = "add_course.html";
+    }
+    else {        
+        var_ref = document.getElementById("var_ref");
+        var_ref.style.backgroundImage = 'url("img/Thumbtack.svg")';
+        var_ref.style.visibility = "visible";
+        var_ref.href = "";
+    }
+}
+
+// Функции на загрузку страницы
+function func_on_load()
+{
+    adaptive();
+    user_status();
+}
+
+// Функции на изменения размера экрана
+function func_on_resize()
+{
+    adaptive();
+}
+
+document.addEventListener("DOMContentLoaded", func_on_load);
+window.addEventListener("resize", func_on_resize);
